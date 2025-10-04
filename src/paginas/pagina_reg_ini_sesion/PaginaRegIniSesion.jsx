f794bf31f509bd4596bbedf94abad00ee99ab5aa
Cajocoza96 <142413770+Cajocoza96@users.noreply.github.com>
2025-10-04 10:48:03 -1200

    11 sol

--- src/paginas/pagina_reg_ini_sesion/PaginaRegIniSesion.jsx
+++ src/paginas/pagina_reg_ini_sesion/PaginaRegIniSesion.jsx
@@ -14,7 +14,7 @@ export default function PaginaRegIniSesion() {
             />
 
             <Cuerpo />
-
+            
             <Footer />
 
         </div>
--- src/paginas/pagina_reg_ini_sesion/footer/Footer.jsx
+++ src/paginas/pagina_reg_ini_sesion/footer/Footer.jsx
@@ -17,7 +17,7 @@ export default function Footer() {
     const rutaDestino = esRegistro ? "/iniciar-sesion" : "/registrar";
 
     return (
-        <div className="w-full p-3 bg-blue-600 dark:bg-black">
+        <div className="w-full p-3 bg-blue-600 dark:bg-black flex-grow flex flex-col">
             <p className="w-full truncate text-sm md:text-base text-white text-center">
                 {textoPregunta} {""}
                 <Link to={rutaDestino} className="cursor-pointer underline hover:opacity-80">
