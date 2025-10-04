ec53de6145ee4e3412f1a4fcfc54c0533c2cac70
Cajocoza96 <142413770+Cajocoza96@users.noreply.github.com>
2025-10-04 10:45:43 -1200

    11 sol

--- src/paginas/pagina_reg_ini_sesion/PaginaRegIniSesion.jsx
+++ src/paginas/pagina_reg_ini_sesion/PaginaRegIniSesion.jsx
@@ -7,7 +7,7 @@ import Footer from "./footer/Footer";
 
 export default function PaginaRegIniSesion() {
     return (
-        <div className="min-h-screen bg-white dark:bg-gray-800 
+        <div className="min-h-[100svh] bg-white dark:bg-gray-800 
                         grid grid-rows-[auto_1fr_auto]">
             <Cabecera
                 paginaRegIniSesion={true}
